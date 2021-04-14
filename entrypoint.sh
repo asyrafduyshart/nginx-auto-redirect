b7017e7a43fb48d9625e96a8e404f95eb6115f61
Asyraf Duyshart <me@asyraf.id>
2021-04-14 13:06:26 +0700

    - init

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ ! -e "/etc/nginx/certs/cert.pem" ] || [ ! -e "/etc/nginx/certs/key.pem" ]
+then
+  openssl req -x509 -newkey rsa:2048 \
+  -subj "/CN=localhost" \
+  -keyout "/etc/nginx/certs/key.pem" \
+  -out "/etc/nginx/certs/cert.pem" \
+  -days 3650 -nodes -sha256
+fi
+
+envsubst '${VERIFICATION_FILE_NAME} ${MOBILE_URL} ${DESKTOP_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+exec "$@"
